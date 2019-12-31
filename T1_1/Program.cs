16e03c338469fd831bea4bf88e79e73124dcf09a
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2019-12-31 20:32:53 +0400

    Создана заготовка проекта.

---
+++ T1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+        }
+
+        static double Add(double first, double second) => first + second;
+    }
+}
